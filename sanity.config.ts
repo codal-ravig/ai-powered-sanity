fe91e41c6766eead23869800ca89db5b45a5d19c
Ravi Gupta <rgupta@codal.com>
2026-02-17 16:09:37 +0530

    feat: bootstrap sanity studio

---
+++ sanity.config.ts
@@ -0,0 +1,18 @@
+import {defineConfig} from 'sanity'
+import {structureTool} from 'sanity/structure'
+import {visionTool} from '@sanity/vision'
+import {schemaTypes} from './schemaTypes'
+
+export default defineConfig({
+  name: 'default',
+  title: 'AI-powered Sanity',
+
+  projectId: '22t68kfp',
+  dataset: 'production',
+
+  plugins: [structureTool(), visionTool()],
+
+  schema: {
+    types: schemaTypes,
+  },
+})
